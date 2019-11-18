9e9ea9649e8090c3be100ea9c0233a49c551d92a
supervii <jwjwshinjwjw@gmail.com>
2019-11-18 16:03:53 +0900

    2019-11-18

---
+++ 06_vue/05_vue_component_with_drf/todo-front/src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <LoginForm/>
+  </div>
+</template>
+
+<script>
+// @ 는 /src의 alias
+// import LoginForm from '../components/LoginForm'
+import LoginForm from '@/components/LoginForm'
+  export default {
+    name: 'login',
+    components: {
+      LoginForm,
+    }
+  }
+</script>
+
+<style>
+
+</style>
