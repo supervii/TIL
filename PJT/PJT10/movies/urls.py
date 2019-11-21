ec14d349f6e598f58a1d098059dc4f2ed3647c8f
supervii <jwjwshinjwjw@gmail.com>
2019-11-21 15:58:28 +0900

    2019-11-21

---
+++ PJT/PJT10/movies/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'movies'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:movie_pk>/', views.detail, name='detail'),
+    path('<int:movie_pk>/reviews/new/', views.review_create, name='review_create'),
+    path('<int:movie_pk>/reviews/delete/<int:review_pk>', views.review_delete, name='review_delete'),
+    path('<int:movie_pk>/like/', views.like, name='like'),
+]
